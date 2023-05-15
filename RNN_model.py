9e44f55a8dba61d03df0fa7f56d2469c082b0a4b
abbexd <47353330+frysd@users.noreply.github.com>
2023-05-15 14:32:17 +0800

    Update RNN_model.py

--- RNN_model.py
+++ RNN_model.py
@@ -1,1 +1,7 @@
-jajajaj
+import torch
+import torch.nn as nn
+
+
+#Example code
+#input_size = 6 because we have 6 features (station_arrival, station_departure, bus_line, current_time, day (mon-sun), month)
+rnn= nn.RNN(input_size=6, hidden_size=1, num_layers = 1, bias = False, batch_first=True)
